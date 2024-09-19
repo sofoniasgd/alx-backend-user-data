96cc9b9f61eeefaa3ff75c2815d253d1f090014f
sofoniasgd <zephaniahsofa@gmail.com>
2024-09-19 03:19:06 +0300

    added task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,13 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - raise 401 error using abort
+    """
+    abort(401, description="Unauthorized")
+    return
